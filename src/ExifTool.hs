marhop/exiftool-haskell

commit 579bf5a1710b4e2294614e11103ce0959b5298b4

Simplify set/deleteMetadata functions

src/ExifTool.hs
@@ -233,24 +233,27 @@ getMetadata et file = do
     options = ["-json", "-a", "-G:0:1", "-s", "-binary"]
 
 -- | Write metadata to a file.
+--
+-- The file is modified in place. Make sure you have the necessary backups!
 setMetadata :: ExifTool            -- ^ ExifTool instance
             -> Metadata            -- ^ tag/value Map
-            -> Text                -- ^ input file name
-            -> Text                -- ^ output file name
+            -> Text                -- ^ file name
             -> IO (Either Text ())
-setMetadata et md infile outfile =
-    withSystemTempFile "exiftool.json" $ \mdfile h -> do
-        hPut h $ encode [delete (Tag "" "" "SourceFile") md]
+setMetadata et m file =
+    withSystemTempFile "exiftool.json" $ \metafile h -> do
+        hPut h $ encode [delete (Tag "" "" "SourceFile") m]
         hFlush h
-        result <-
-            sendCommand et [infile, "-json=" <> cs mdfile, "-f", "-o", outfile]
+        result <- sendCommand et (file : "-json=" <> cs metafile : options)
         return $ const () <$> result
+  where
+    options = ["-overwrite_original", "-f"]
 
 -- | Delete metadata from a file.
+--
+-- The file is modified in place. Make sure you have the necessary backups!
 deleteMetadata :: ExifTool            -- ^ ExifTool instance
                -> [Tag]               -- ^ tags to be deleted
-               -> Text                -- ^ input file name
-               -> Text                -- ^ output file name
+               -> Text                -- ^ file name
                -> IO (Either Text ())
 deleteMetadata et ts = setMetadata et (fromList $ fmap (, String "-") ts)
 

commit a1d1d631022e662119f4cce5568b8313a30885d3

Bugfix regarding ExifTool output
Bug introduced by commit 579bf5a1710b4e2294614e11103ce0959b5298b4, the
-overwrite_original option changes the ExifTool output message.

src/ExifTool.hs
@@ -222,7 +222,7 @@ sendCommand (ET i o e _) cmds = do
                 readErr h (acc <> l)
     -- | Make sure an error string actually counts as error.
     isError :: Text -> Bool
-    isError t = t `notElem` ["", "    1 image files created"]
+    isError t = t `notElem` ["", "    1 image files updated"]
 
 -- | Read all metadata from a file, with ExifTool errors leading to runtime
 -- errors. (Use 'getMetaEither' instead if you would rather intercept them.)